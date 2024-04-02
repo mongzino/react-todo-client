da578318d574cfc30850a547794f9ab9ef8e136d
mongzino <112944480+mongzino@users.noreply.github.com>
2024-04-02 11:16:49 +0900

    fix

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,9 @@ export default function Home() {
 
   useEffect(() => {
     async function getData() {
-      const result = await axios.get("http://localhost:3001/get");
+      const result = await axios.get(
+        "https://react-todo-server-pukyong-national-university.koyeb.app/8000/get"
+      );
       setTodos(result.data);
       inputRef.current.focus();
     }
@@ -18,22 +20,37 @@ export default function Home() {
   }, []);
 
   const handleAdd = async () => {
-    await axios.post("http://localhost:3001/add", { task: task });
-    const result = await axios.get("http://localhost:3001/get");
+    await axios.post(
+      "https://react-todo-server-pukyong-national-university.koyeb.app/8000/add",
+      { task: task }
+    );
+    const result = await axios.get(
+      "https://react-todo-server-pukyong-national-university.koyeb.app/8000/get"
+    );
     setTodos(result.data);
     inputRef.current.value = "";
     inputRef.current.focus();
   };
 
   const handleEdit = async (id) => {
-    await axios.put("http://localhost:3001/update/" + id);
-    const result = await axios.get("http://localhost:3001/get");
+    await axios.put(
+      "https://react-todo-server-pukyong-national-university.koyeb.app/8000/update/" +
+        id
+    );
+    const result = await axios.get(
+      "https://react-todo-server-pukyong-national-university.koyeb.app/8000/get"
+    );
     setTodos(result.data);
   };
 
   const handleDelete = async (id) => {
-    await axios.delete("http://localhost:3001/delete/" + id);
-    const result = await axios.get("http://localhost:3001/get");
+    await axios.delete(
+      "https://react-todo-server-pukyong-national-university.koyeb.app/8000/delete/" +
+        id
+    );
+    const result = await axios.get(
+      "https://react-todo-server-pukyong-national-university.koyeb.app/8000/get"
+    );
     setTodos(result.data);
   };
 
